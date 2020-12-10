34c5249d2d1c045ab81cbd482aca0b0bc8d3cc75
sunnylearne <ikesundayshield@gmail.com>
2020-12-10 10:03:53 +0100

    just started out new chat app with php tutorial

---
+++ signin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="signin-form">
+        <form action="" method="post">
+            <div class="form-header">
+                <h2>Sign In</h2>
+                <p>Log in to MyChat</p>
+            </div>
+
+            <div class="form-group">
+                <label for="">Email</label>
+                <input class="form-control" type="email" name="email" id="" placeholder="someone@site.com">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
