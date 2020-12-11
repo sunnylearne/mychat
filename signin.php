7c99724c695a029f7368d48f6e8aee006c480ed5
sunnylearne <ikesundayshield@gmail.com>
2020-12-11 02:30:00 +0100

    connected to local database and inserted data through our form into database

--- signin.php
+++ signin.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/signin.css">
 </head>
 <body>
@@ -31,7 +32,7 @@
             </div>
         </form>
 
-        <div class="text-center small" style="color: #67428B";>Don't have an account <a href="signup.php">Create One</a></div>
+        <div class="text-center small" style="color: #67428B";>Don't have an account? <a href="signup.php">Create One</a></div>
     </div>
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create new accout</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/signup.css">
 </head>
+
 <body>
     <div class="signup-form">
-        <form action="" method="post">
+        <form action="signup_user.php" method="POST">
             <div class="form-header">
                 <h2>Sign Up</h2>
                 <p>Fill out this form and start chatting with your friends</p>
@@ -31,41 +34,39 @@
 
             <div class="form-group">
                 <label for="">Country</label>
-               <select name="user_country" class="form-control" requred>
-                   <option disabled="">Select a Country</option>
-                   <option>Nigeria</option>
-                   <option>United states of America</option>
-                   <option>India</option>
-                   <option>Ghana</option>
-                   <option>France</option>
-               </select>
+                <select name="user_country" class="form-control" requred>
+                    <option disabled="">Select a Country</option>
+                    <option>Nigeria</option>
+                    <option>United states of America</option>
+                    <option>India</option>
+                    <option>Ghana</option>
+                    <option>France</option>
+                </select>
             </div>
 
             <div class="form-group">
                 <label for="">Gender</label>
-               <select name="user_gender" class="form-control" requred>
-                   <option disabled="">Select your Gender</option>
-                   <option>Male</option>
-                   <option>Female</option>
-                   <option>Others</option>
-               </select>
+                <select name="user_gender" class="form-control" requred>
+                    <option disabled="">Select your Gender</option>
+                    <option>Male</option>
+                    <option>Female</option>
+                    <option>Others</option>
+                </select>
             </div>
 
             <div class="form-group">
                 <label class="checkbox-inline"><input type="checkbox" required> I accept the <a href="#">Terms of Use</a>&amp; <a href="#">Privacy Policy</a></label>
             </div>
 
-            <div class="form-group">
-                <label for="">Password</label>
-                <input class="form-control" type="password" name="password" id="" placeholder="Password" autocomplete="0ff" required>
-            </div>
 
             <div class="form-group">
-                 <button type="submit" class="btn btn-primary btn-block btn-lg" name="sign_up">Sign Up</button>
+                <button type="submit" class="btn btn-primary btn-block btn-lg" name="sign_up">Sign Up</button>
             </div>
+            <?php require_once("signup_user.php"); ?>
         </form>
 
-        <div class="text-center small" style="color: #67428B";>Already have an account? <a href="signin.php">Sign in here</a></div>
+        <div class="text-center small" style="color: #67428B" ;>Already have an account? <a href="signin.php">Sign in here</a></div>
     </div>
 </body>
+
 </html>
